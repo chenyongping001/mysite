e3e8bffedbc736362e4ab7869b2f25b48639f470
chenyongping <chenyongping001@gmail.com>
2021-08-28 15:11:39 +0800

    add sitemap and RSS function

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
-from django.contrib import admin
+from django.contrib import admin, sitemaps
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 
+sitemaps = {
+    "post": PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {
+        'sitemaps': sitemaps
+    }, name='django.contrib.sitemaps.views.sitemap')
 ]
